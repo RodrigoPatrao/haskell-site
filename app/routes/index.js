91e8e30d352b081635d4d7fa2c81d489a572041a
RodrigoPatrao <rodrigo.viveiros@outlook.com>
2017-07-07 18:03:19 -0300

    Liberação do app, a rota para login bloqueava a visualização do mesmo.

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,16 +10,20 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-  request.post({url:'https://locadoura-veiculos-cannarozzo.c9users.io/login',
-                form: req.body },
-                function(err,httpResponse,body){
-                  if (!err && res.statusCode === 200) {
-                    req.app.locals.currentUser = req.body.login;
-                    res.json(req.body);
-                  } else {
-                    res.redirect('/');
-                  }
-                });
+  if (!req.app.locals.currentUser) {
+    req.app.locals.currentUser = req.body.login;
+    res.redirect('/rental/new');
+  }
+  // request.post({url:'https://locadoura-veiculos-cannarozzo.c9users.io/login',
+  //               form: req.body },
+  //               function(err,httpResponse,body){
+  //                 if (!err && res.statusCode === 200) {
+  //                   req.app.locals.currentUser = req.body.login;
+  //                   res.json(req.body);
+  //                 } else {
+  //                   res.redirect('/');
+  //                 }
+  //               });
 });
 
 router.get('/logoff', function(req, res){
